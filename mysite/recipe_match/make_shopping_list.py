29a88cc2fbfc397d1387021594b21bd8a0f1fe09
Monica Mashock <midgica@gmail.com>
2021-01-06 18:18:02 -0800

    trying to write tests

--- mysite/recipe_match/make_shopping_list.py
+++ mysite/recipe_match/make_shopping_list.py
@@ -1,5 +1,5 @@
 from .models import Food, Ingredient, Menu, Recipe, Selection, Shopping_List, Unit
-
+from .convert_servings import convert_servings
 
 def make_shopping_list(menu):
 
@@ -10,21 +10,46 @@ def make_shopping_list(menu):
     #break recipes into individual ingredients
     all_ingredients = {}
     duplicate_ingredients = {}
-    for each in selections:
-        for ingred in Ingredient.objects.filter(recipe = each.recipe):
+    for sel in selections:
+        #convert ingredients by number of servings
+        ingredient_list = convert_servings(sel.recipe, sel.desired_servings)
+        for ingred in ingredient_list:
             try:
+                #if there's already a value for this ingredient
                 if all_ingredients[ingred.food]:
-                    ing_tuple = all_ingredients[ingred.food]
-                    for amt, unit in ing_tuple:
-                        #if ingredient units match, add them together 
-                        if ingred.unit == unit:
-                            new_amt = amt + ingred.amount
-                            all_ingredients[ingred.food] = (new_amt, unit)
-                        #otherwise append more values
-                        else:
-                            all_ingredients.setdefault(key, [])
-                            all_ingredients[ingred.food].append(ingred.amount,
-                                                                ingred.unit)
+                    #if there's only one tuple there
+                    try:
+                        len(all_ingredients[ingred.food]) == 2
+                        for amt, unit in all_ingredients[ingred.food]:
+                            #if units match, add them together
+                            if ingred.unit == unit:
+                                new_amt = amt + ingred.amount
+                                all_ingredients[ingred.food] = (new_amt, unit)
+                            #otherwise append more values
+                            else:
+                                all_ingredients.setdefault(key, [])
+                                all_ingredients[ingred.food].append(ingred.amount,
+                                                                    ingred.unit)
+
+                    except:  #if there's more than one tuple
+                        ing_tuple_list = all_ingredients[ingred.food]
+                        for each in ing_tuple_list:
+                            (amt, unit) = ing_tuple_list[0]
+                            #if ingredient units match, add them together
+                            if ingred.unit == unit:
+                                new_amt = amt + ingred.amount
+                                all_ingredients[ingred.food] = (new_amt, unit)
+                                amt = None
+                                unit = None
+                        #if there weren't any matches in the for loop
+                        #append more values. if there were matches, append none
+                            else:
+                                try:
+                                    all_ingredients.setdefault(key, [])
+                                    all_ingredients[ingred.food].append(ingred.amount,
+                                                                        ingred.unit)
+                                except: #if appending none produces error
+                                    None
             except:
                 all_ingredients[ingred.food] = (ingred.amount, ingred.unit)
 
@@ -36,8 +61,6 @@ def make_shopping_list(menu):
             unit = None
             try:
                 (amt, unit) = tup
-                print(amt)
-                print(unit)
             except ValueError:
                 (amt) = tup
         shopping_list.append(Ingredient.objects.create(food = ingred,
--- mysite/recipe_match/tests.py
+++ mysite/recipe_match/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Recipe, Selection, Menu
 
-# Create your tests here.
+
+class Make_Shopping_List_Tests(TestCase):
+
+
+    def test_make_shopping_list_with_no_repeated_ingredients(self):
+        recipe1 = Recipe.objects.get(name = "Moo Shu Pork Tacos")
+        recipe2 = Recipe.objects.get(name = "Cream Cheese Pancakes")
+        my_menu = Menu.objects.create()
+        selection1 = Selection.objects.create(menu = my_menu,
+                                              recipe = recipe1,
+                                              desired_servings = 8)
+        selection2 = Selection.objects.create(menu = my_menu,
+                                              recipe = recipe2,
+                                              desired_servings = 8)
+        shopping_list = make_shopping_list(menu)        
+        #this should return a shopping list with 18 ingredients
+        self.assertIs(len(shopping_list), 18)
+
+
+
+#make_shopping_list_with_repeated_ingredients
+
+#make_shopping_list_with_duplicate_recipes
+
+#make_shopping_list_with_default_servings
+
+#make_shopping_list_with_desired_servings
